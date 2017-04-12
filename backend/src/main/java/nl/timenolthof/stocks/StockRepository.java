6b9d744c6862042226db8ca328925081c89d4e06
Timen Olthof <timen@timenolthof.nl>
2017-04-12 17:32:30 +0200

    chore: backend (pact provider) including verification

---
+++ backend/src/main/java/nl/timenolthof/stocks/StockRepository.java
@@ -0,0 +1,12 @@
+package nl.timenolthof.stocks;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface StockRepository extends CrudRepository<Stock, Long> {
+
+}
---
+++ backend/src/main/java/nl/timenolthof/stocks/StockRepositoryConfiguration.java
@@ -0,0 +1,14 @@
+package nl.timenolthof.stocks;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class StockRepositoryConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Stock.class);
+    }
+}
